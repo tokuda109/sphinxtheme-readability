90d7c66fe9a847d066939655b742b4f3f13b42c7
tokuda109 <tokuda109@gmail.com>
2012-06-16 02:27:23 +0900

    A few updated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sphinx.themes.readability',
     version='0.0.1',
-    url='',
-    license='',
+    url='http://sphinx-themes-readability.readthedocs.org/',
+    license='BSD',
     author='Tsuyoshi Tokuda',
     author_email='tokuda109@gmail.com',
     description='',
     long_description='',
-    packages=[],
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'docutils',
+        'sphinx',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Text Processing :: Markup'
+    ]
+    
 )
