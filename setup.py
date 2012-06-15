f9bb25cb5e64d4d5b35809c1025a3dadb2872b4a
tokuda109 <tokuda109@gmail.com>
2012-06-16 01:44:19 +0900

    Added setup.py in layout

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='sphinx.themes.readability',
+    version='0.0.1',
+    url='',
+    license='',
+    author='Tsuyoshi Tokuda',
+    author_email='tokuda109@gmail.com',
+    description='',
+    long_description='',
+    packages=[],
+)
