bdaab1d89d57cb8dd02fc4f6ed5f0bfecf34bd22
tokuda109 <tokuda109@gmail.com>
2012-06-18 03:45:04 +0900

    A few widgets is coded

--- docs/conf.py
+++ docs/conf.py
@@ -135,7 +135,10 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {
+    'index': ['sidebar-index.html', 'sourcelink.html', 'searchbox.html'],
+    '**':    ['localtoc.html', 'sourcelink.html', 'searchbox.html']
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
